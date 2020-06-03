1bfc4dac61a5c73f1785bd610cf44a00646afa38
Christian <34753517+Chrausen@users.noreply.github.com>
2020-06-03 17:28:22 +0200

    added example test

---
+++ src/test/java/exampleTest.java
@@ -0,0 +1,23 @@
+import com.discord.bot.GoogleCalendarManager;
+import com.discord.bot.data.UserData;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
+public class exampleTest {
+
+    UserData user = new UserData("1234");
+
+
+    @Test
+    public void simpleCheck() {
+
+        String[] interests = new String[1];
+        interests[0] = "blubbern";
+        user.setInterests(interests);
+
+
+        Assert.assertEquals(user.getInterests()[0],"blubbern");
+    }
+}
