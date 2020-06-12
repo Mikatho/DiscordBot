9c7553d91415a4df9a10dd80d3b65dd0c340052d
Christian <34753517+Chrausen@users.noreply.github.com>
2020-06-12 10:28:21 +0200

    new tests

--- src/test/java/com/discord/bot/data/MeetingDataTest.java
+++ src/test/java/com/discord/bot/data/MeetingDataTest.java
@@ -1,45 +1,75 @@
 package com.discord.bot.data;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class MeetingDataTest {
 
-    MeetingData testMeeting = new MeetingData("hostID", "participantID", 123, 456);
-    MeetingData testMeetingOverloaded = new MeetingData("hostID", "participantID", 12, 34, "message"); //Overloaded constructor
+
+    private static MeetingData testMeeting;
+    private static MeetingData testMeeting2;
+    private static MeetingData testMeetingOverloaded;
+    private static MeetingData testMeetingOverloaded2;
+
+    @BeforeClass
+    public static void createObject()
+    {
+        /**
+         * Create unequal objects
+         */
+        testMeeting = new MeetingData("hostID", "participantID", 123, 456);
+        testMeeting2 = new MeetingData("hostID", "participantID", 456, 789);
+
+        testMeetingOverloaded = new MeetingData("hostID", "participantID", 12, 34, "message");  //Overloaded constructor
+        testMeetingOverloaded2 = new MeetingData("hostID", "participantID", 34, 56, "message2"); //Overloaded constructor
+    }
 
     @Test
     public void meetingSetterGetterTest() {
 
         Assert.assertEquals(testMeeting.getParticipantID(),"participantID");
-        Assert.assertEquals(testMeeting.getStartTime(),123);
-        Assert.assertEquals(testMeeting.getEndTime(),456);
+        Assert.assertEquals(testMeeting.getStarttime(),123);
+        Assert.assertEquals(testMeeting.getEndtime(),456);
 
         testMeeting.setParticipantID("test");
-        testMeeting.setStartTime(456);
-        testMeeting.setEndTime(789);
+        testMeeting.setStarttime(456);
+        testMeeting.setEndtime(789);
+        testMeeting.setMeetingID(1);
+        testMeeting.setUserID("22");
 
         Assert.assertEquals(testMeeting.getParticipantID(),"test");
-        Assert.assertEquals(testMeeting.getStartTime(),456);
-        Assert.assertEquals(testMeeting.getEndTime(),789);
+        Assert.assertEquals(testMeeting.getStarttime(),456);
+        Assert.assertEquals(testMeeting.getEndtime(),789);
+        Assert.assertEquals(testMeeting.getMeetingID(),1);
+        Assert.assertEquals(testMeeting.getUserID(),"22");
 
         /* Overloaded constructor */
 
         Assert.assertEquals(testMeetingOverloaded.getParticipantID(),"participantID");
-        Assert.assertEquals(testMeetingOverloaded.getStartTime(),12);
-        Assert.assertEquals(testMeetingOverloaded.getEndTime(),34);
+        Assert.assertEquals(testMeetingOverloaded.getStarttime(),12);
+        Assert.assertEquals(testMeetingOverloaded.getEndtime(),34);
         Assert.assertEquals(testMeetingOverloaded.getMessage(), "message");
 
         testMeetingOverloaded.setParticipantID("newParticipantID");
-        testMeetingOverloaded.setStartTime(34);
-        testMeetingOverloaded.setEndTime(56);
+        testMeetingOverloaded.setStarttime(34);
+        testMeetingOverloaded.setEndtime(56);
         testMeetingOverloaded.setMessage("newMessage");
 
         Assert.assertEquals(testMeetingOverloaded.getParticipantID(),"newParticipantID");
-        Assert.assertEquals(testMeetingOverloaded.getStartTime(),34);
-        Assert.assertEquals(testMeetingOverloaded.getEndTime(),56);
+        Assert.assertEquals(testMeetingOverloaded.getStarttime(),34);
+        Assert.assertEquals(testMeetingOverloaded.getEndtime(),56);
         Assert.assertEquals(testMeetingOverloaded.getMessage(), "newMessage");
     }
 
+    /**
+     * Check assertion
+     */
+    @Test
+    public void equalsForDiffObjectsTest(){
+        Assert.assertNotEquals(testMeeting, testMeeting2);
+        Assert.assertNotEquals(testMeetingOverloaded, testMeetingOverloaded2);  // Overloaded
+    }
 }
--- src/test/java/com/discord/bot/data/UserDataTest.java
+++ src/test/java/com/discord/bot/data/UserDataTest.java
@@ -1,16 +1,40 @@
 package com.discord.bot.data;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 
 public class UserDataTest {
 
-    UserData testUser = new UserData("1234");
+    private static UserData testUser;
+    private static UserData testUser2;
 
-    String[] testInterests = {"lala", "lili"};
-    String[] testCompetencies = {"lala", "lulu"};
-    String testAddress = "straße 22";
+    private static String testInterests;
+    private static String testCompetencies;
+    private static String testAddress;
+    private static String testgCalendarLink;
+
+    @BeforeClass
+    public static void create() {
+
+        /**
+         * Create unequal objects
+         */
+        testUser = new UserData("1234");
+        testUser2 = new UserData("5678");
+
+        testInterests = "lala";
+        testCompetencies = "lulu";
+        testAddress = "straße 22";
+        testgCalendarLink = "www.gCalendar.com";
+    }
 
     @Test
     public void userSetterGetterTest() {
@@ -18,14 +42,33 @@ public class UserDataTest {
         testUser.setInterests(testInterests);
         testUser.setCompetencies(testCompetencies);
         testUser.setAddress(testAddress);
+        testUser.setgCalendarLink(testgCalendarLink);
 
-        Assert.assertEquals(testUser.getInterests()[0],"lala");
-        Assert.assertEquals(testUser.getInterests()[1],"lili");
-
-        Assert.assertEquals(testUser.getCompetencies()[0],"lala");
-        Assert.assertEquals(testUser.getCompetencies()[1],"lulu");
-
+        Assert.assertEquals(testUser.getInterests(),"lala");
+        Assert.assertEquals(testUser.getCompetencies(),"lulu");
         Assert.assertEquals(testUser.getAddress(),"straße 22");
+        Assert.assertEquals(testUser.getgCalendarLink(), "www.gCalendar.com");
+
+        testUser.setInterests("asd");
+        testUser.setCompetencies("qwe");
+        testUser.setAddress("straße 99");
+        testUser.setgCalendarLink("www.NewgCalendar.com");
+
+        Assert.assertEquals(testUser.getInterests(),"asd");
+        Assert.assertEquals(testUser.getCompetencies(),"qwe");
+        Assert.assertEquals(testUser.getAddress(),"straße 99");
+        Assert.assertEquals(testUser.getgCalendarLink(), "www.NewgCalendar.com");
+
+        Assert.assertEquals(testUser.getUserID(),"1234" );
+        Assert.assertEquals(testUser2.getUserID(),"5678" );
+    }
+
+    @Test
+    public void equalsForDiffObjectsTest(){
+    /**
+    * Check assertion
+    */
+        Assert.assertNotEquals(testUser, testUser2);
     }
 
 }
